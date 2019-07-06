6ba1df9d03464dc3acf90adb1bea43a7861fe19c
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-07-06 23:44:11 +0900

    add firebase settings

---
+++ src/plugins/firebase.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import VueFire from 'vuefire'
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+Vue.use(VueFire)
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyBNF2ZbkSrmPVdtx8rAGrcfV3j_5AomLVc",
+  authDomain: "vue-chat-a52ef.firebaseapp.com",
+  databaseURL: "https://vue-chat-a52ef.firebaseio.com",
+  projectId: "vue-chat-a52ef",
+  storageBucket: "vue-chat-a52ef.appspot.com",
+  messagingSenderId: "60853095926",
+  appId: "1:60853095926:web:9acbad103c71116f"
+});
+
+export const db = firebaseApp.firestore();
